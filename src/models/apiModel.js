047c61afb879bf19008741830d1ce50d74ff39b6
Pedro Ávila <pedro2003dressler@gmail.com>
2022-08-24 20:09:05 -0300

    Deletando função inutilizada

--- src/models/apiModel.js
+++ src/models/apiModel.js
@@ -7,11 +7,6 @@ export async function rFile() {
   return data;
 }
 
-export async function rGrades(){
-  let data = await rFile();
-  data = data.grades.map(item => {return { student: item.student, value: item.value }});
-}
-
 export async function wFile(file){
   await fs.writeFile(fileName, JSON.stringify(file, null, 2),  (err) => { if(err) throw err })
 }
